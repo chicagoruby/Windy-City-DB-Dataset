c6c6ae4c17fac97acc3979dc512280c85f9659c7
Chris Anderson <jchris@apache.org>
2010-06-11 13:50:22 +0800

    denormalize user info into the actual posts, answers, and comments

--- to_couch.rb
+++ to_couch.rb
@@ -7,20 +7,29 @@ require 'couchrest'
 
 
 DB = CouchRest::database!("windy-city")
-SLICE_SIZE = 1000
 
-["users", "posts", "answers", "comments"].each do |set|
+puts "loading users into Ruby"
+
+users = JSON.parse(open(File.join(File.dirname(__FILE__), "data", "users.json")).read)["users"]
+user_map = {}
+users.each do |u|
+  user_map[u["Id"]] = u
+end
+
+["posts", "answers", "comments"].each do |set|
   puts "loading #{set} into ruby"
   file = open(File.join(File.dirname(__FILE__), "data", "#{set}.json")).read
   items = JSON.parse(file)[set].map do |item|
     item["type"] = set
+    if set == "comments"
+      item["user"] = user_map[item["UserId"]]
+    else
+      item["user"] = user_map[item["OwnerUserId"]]
+    end
     item
   end
-  puts "loading #{set} into CouchDB -- #{items.length} documents in slices of #{SLICE_SIZE}"
-  items.each_slice(SLICE_SIZE) do |slice|
-    DB.bulk_save(slice)
-    putc "."
-  end
+  puts "loading #{set} into CouchDB -- #{items.length} documents"
+  DB.bulk_save(items)
   puts "done with #{set}"
 end
 
