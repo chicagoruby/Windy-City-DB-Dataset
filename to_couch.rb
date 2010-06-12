6375bea83f2aa375ca2147aa1dd2761838b1ee9e
Chris Anderson <jchris@apache.org>
2010-06-12 13:03:48 +0800

    more realistic docs

--- to_couch.rb
+++ to_couch.rb
@@ -4,7 +4,8 @@ require 'couchrest'
 # note: this might require Ruby 1.9 (or you might need to supply the Array#each_slice function, which should be simple to implement if your ruby doesn't have it)
 
 # hacked up in about 10 minutes by Chris Anderson - @jchris
-
+# correction it took me more like 30 once I added the comments and denormalized the usernames.
+# but I'm slow at git so it actually took longer. that's life.
 
 DB = CouchRest::database!("windy-city")
 
